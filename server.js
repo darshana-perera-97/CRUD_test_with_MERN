5d88e64c7e1422d7b12160455aced98b92a88d39
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-11 22:42:14 +0530

    server.js modified

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.json())
+app.use(Cors());
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connecte"))
+  .then(() => console.log("MongoDB connected"))
   .catch((err)=>console.log(err));
 
   app.listen(5000,()=>{
