0b4c4ccd5cc750058f24f5bc8642e78a059cf413
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-12 07:53:21 +0530

    routes created in TodoRoutes.js

--- routes/ToDoRoutes.js
+++ routes/ToDoRoutes.js
@@ -2,4 +2,12 @@ const {Router} = require("express");
 
 const router = Router();
 
-router.get("/get-todo", getToDo)
+const {getToDo,saveToDo,deleteToDo,updateToDo} = require("../controllers/ToDoController")
+
+router.get("/get-todo", getToDo);
+router.get("/save-todo", saveToDo);
+router.get("/delete-todo", deleteToDo);
+router.get("/update-todo", updateToDo);
+
+
+module.exports = router;
