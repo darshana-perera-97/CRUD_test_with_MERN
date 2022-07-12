9bafd3aa7ec8dca16a54f9643df3060dc49ba7c7
Darshana Perera <darshana.saluka.10@gmail.com>
2022-07-12 07:50:39 +0530

    controllers created in ToDoModels.js

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,1 +1,30 @@
 const ToDoModule = require("../models/ToDoModels");
+
+module.exports.getToDo = async (req, res) => {
+  const Todo = await TodoModel.find();
+  res.send(Todo);
+};
+
+module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
+
+  TodoModel.create({ text })
+    .then(() => res.set(201).send("Added succesfully.."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+
+  TodoModel.findByIdAndRemove(id)
+    .then(() => res.set(201).send("Deleted succesfully.."))
+    .catch((err) => console.log(err));
+};
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+
+  TodoModel.findByIdAndUpdate(id, { text })
+    .then(() => res.set(201).send("Updated succesfully.."))
+    .catch((err) => console.log(err));
+};
